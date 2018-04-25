4b5647cdcd6d7f06e0ca4b1ff09c3ea2e10b175f
Marc LEONETTI <mleonett@e1r8p12.42.fr>
2018-04-25 11:56:44 +0200

    str_isascii normed

--- ft_str_isascii.c
+++ ft_str_isascii.c
@@ -6,7 +6,7 @@
 /*   By: mleonett <mleonett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/05 15:01:59 by mleonett          #+#    #+#             */
-/*   Updated: 2018/04/05 15:07:41 by mleonett         ###   ########.fr       */
+/*   Updated: 2018/04/25 11:56:17 by mleonett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@ int		ft_str_isascii(char *str)
 {
 	while (*str)
 	{
-		if (!(0 <= *str)) 
+		if (!(0 <= *str))
 			return (0);
 		str++;
 	}
